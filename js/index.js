4cea1d6be80d30e0f71150a3691df0b8de5a73a9
chloe toussaint <toussaintchloe@hotmail.fr>
2021-03-27 15:04:55 +0100

    fleche ok + video background

--- js/index.js
+++ js/index.js
@@ -1,59 +1,92 @@
-window.addEventListener("DOMContentLoaded", (event) => {
+//Récuperation du contexte du canvas
+var canvas = document.getElementById("player1");
+var ctx = canvas.getContext("2d");
+const X = canvas.width;
+const Y = canvas.height;
 
-    var canvas = document.getElementById("example");
-    var ctx = canvas.getContext("2d");
-    const X = canvas.width;
-    const Y = canvas.height;
+//chargement des fleches mouvantes 
+var arrowDown = new Image();
+arrowDown.src = "images/arrow-down.png";
+var arrowLeft = new Image();
+arrowLeft.src = "images/arrow-left.png";
+var arrowUp = new Image();
+arrowUp.src = "images/arrow-up.png";
+var arrowRight = new Image();
+arrowRight.src = "images/arrow-right.png";
+var movingArrowsImages = [arrowDown, arrowLeft, arrowUp, arrowRight];
 
-    var arrowDown = new Image();
-    arrowDown.src = "images/arrow-down.jpeg";
-    var arrowLeft = new Image();
-    arrowLeft.src = "images/arrow-left.jpeg";
-    var arrowUp = new Image();
-    arrowUp.src = "images/arrow-up.jpeg";
-    var arrowRight = new Image();
-    arrowRight.src = "images/arrow-right.jpeg";
+// chargement fleches fixes
+var arrowDownFaded = new Image();
+arrowDownFaded.src = "images/arrow-down-faded.png";
+var arrowLeftFaded = new Image();
+arrowLeftFaded.src = "images/arrow-left-faded.png";
+var arrowUpFaded = new Image();
+arrowUpFaded.src = "images/arrow-up-faded.png";
+var arrowRightFaded = new Image();
+arrowRightFaded.src = "images/arrow-right-faded.png";
 
-    var images = [arrowDown, arrowLeft, arrowUp, arrowRight];
-    var imageCount = images.length;
-    var imagesLoaded = 0;
+//démarrage du jeu lorsque toutes les images sont "loaded"
+var images = [arrowDown, arrowLeft, arrowUp, arrowRight, arrowDownFaded, arrowLeftFaded, arrowUpFaded, arrowRightFaded];
+var imageCount = images.length;
+var imagesLoaded = 0;
 
-    for (var i = 0; i < imageCount; i++) {
-        images[i].onload = function () {
-            imagesLoaded++;
-            if (imagesLoaded == imageCount) {
-                updateCanvas();
-            }
+for (var i = 0; i < imageCount; i++) {
+    images[i].onload = function () {
+        imagesLoaded++;
+        if (imagesLoaded == imageCount) {
+            // updateCanvas();
         }
     }
+}
 
-
-
-
-    class Arrow {
-        constructor(direction, position) {
-            this.direction = direction;
-            this.y = Y + position * 100;
-        }
-        move() {
-            this.y = this.y - 7;
-            ctx.drawImage(images[this.direction], this.direction * 100, this.y, 80, 80);
-        }
+//Définition de la class "arrow"
+class Arrow {
+    constructor(direction, position) {
+        this.direction = direction;
+        this.y = Y + position * 100;
     }
-
-
-
-    let arrows = [];
-    for ( var i = 0; i < 100; i ++) {
-       let arrow = new Arrow (Math.floor (Math.random()*images.length) , i);
-       arrows.push (arrow);
+    move() {
+        this.y = this.y - 4;
+        ctx.drawImage(movingArrowsImages[this.direction], this.direction * 100, this.y, 80, 80);
     }
+}
 
-    function updateCanvas() {
-        ctx.clearRect(0, 0, X, Y);
-        arrows.forEach(arrow => arrow.move());
-        requestAnimationFrame(updateCanvas);
+//création des fleches de la partie en aleatoire
+let arrows = [];
+for (var i = 0; i < 100; i++) {
+    let arrow = new Arrow(Math.floor(Math.random() * movingArrowsImages.length), i);
+    arrows.push(arrow);
+}
+
+//
+var startButton = document.getElementById("start-button");
+startButton.onclick = startStop;
+var animationFrameId ;
+
+function startStop() {
+    if (startButton.innerHTML == "Stop") {
+        startButton.innerHTML = "Start";
+        window.cancelAnimationFrame (animationFrameId);
     }
+    else {
+        startButton.innerHTML = "Stop";
+        updateCanvas ();
+    }
+}
+
+function updateCanvas() {
+    ctx.clearRect(0, 0, X, Y);
+    drawArrowFixed();
+    arrows.forEach(arrow => arrow.move());
+    animationFrameId = requestAnimationFrame(updateCanvas);
+}
+
+function drawArrowFixed() {
+    ctx.drawImage(arrowDownFaded, 0, 0, 80, 80);
+    ctx.drawImage(arrowLeftFaded, 100, 0, 80, 80);
+    ctx.drawImage(arrowUpFaded, 200, 0, 80, 80);
+    ctx.drawImage(arrowRightFaded, 300, 0, 80, 80);
+}
 
 
-});
+
