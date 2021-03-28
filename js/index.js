7df0c384e18c7927a5c2771906e3d001db323624
chloe toussaint <toussaintchloe@hotmail.fr>
2021-03-28 20:38:45 +0200

    affichage score

--- js/index.js
+++ js/index.js
@@ -6,28 +6,20 @@ const Y = canvas.height;
 const video = document.getElementById("video");
 
 //chargement des fleches mouvantes 
-var arrowDown = new Image();
-arrowDown.src = "images/arrow-down.png";
-var arrowLeft = new Image();
-arrowLeft.src = "images/arrow-left.png";
-var arrowUp = new Image();
-arrowUp.src = "images/arrow-up.png";
-var arrowRight = new Image();
-arrowRight.src = "images/arrow-right.png";
-var movingArrowsImages = [arrowDown, arrowLeft, arrowUp, arrowRight];
+var movingArrowsImages = ["arrow-down", "arrow-left", "arrow-up", "arrow-right"].map(name => createImage(name));
 
 // chargement fleches fixes
-var arrowDownFaded = new Image();
-arrowDownFaded.src = "images/arrow-down-faded.png";
-var arrowLeftFaded = new Image();
-arrowLeftFaded.src = "images/arrow-left-faded.png";
-var arrowUpFaded = new Image();
-arrowUpFaded.src = "images/arrow-up-faded.png";
-var arrowRightFaded = new Image();
-arrowRightFaded.src = "images/arrow-right-faded.png";
+var staticArrowsImages = ["arrow-down-faded", "arrow-left-faded", "arrow-up-faded", "arrow-right-faded"].map(name => createImage(name));
+
+function createImage(name) {
+    var image = new Image();
+    image.src = `images/${name}.png`;
+    return image
+}
+
 
 //démarrage du jeu lorsque toutes les images sont "loaded"
-var images = [arrowDown, arrowLeft, arrowUp, arrowRight, arrowDownFaded, arrowLeftFaded, arrowUpFaded, arrowRightFaded];
+var images = movingArrowsImages.concat(staticArrowsImages);
 var imageCount = images.length;
 var imagesLoaded = 0;
 
@@ -84,27 +76,28 @@ function updateCanvas() {
 }
 
 function drawArrowFixed() {
-    ctx.drawImage(arrowDownFaded, 0, 0, 80, 80);
-    ctx.drawImage(arrowLeftFaded, 100, 0, 80, 80);
-    ctx.drawImage(arrowUpFaded, 200, 0, 80, 80);
-    ctx.drawImage(arrowRightFaded, 300, 0, 80, 80);
+    ctx.drawImage(staticArrowsImages[0], 0, 0, 80, 80);
+    ctx.drawImage(staticArrowsImages[1], 100, 0, 80, 80);
+    ctx.drawImage(staticArrowsImages[2], 200, 0, 80, 80);
+    ctx.drawImage(staticArrowsImages[3], 300, 0, 80, 80);
 }
 
+
+var score = 0;
 document.body.addEventListener("keydown", event => {
     var currentArrow = arrows.filter(a => a.y > 0 && a.y < 80)[0];
-    if (currentArrow && currentArrow.direction === 0 && event.code === 'ArrowDown') {
-        console.log("success");
-    } else if (currentArrow && currentArrow.direction === 1 && event.code === 'ArrowLeft') {
-        console.log("success");
-    } else if (currentArrow && currentArrow.direction === 2 && event.code === 'ArrowUp') {
-        console.log("success");
-    } else if (currentArrow && currentArrow.direction === 3 && event.code === 'ArrowRight') {
-        console.log("success");
+    if (currentArrow && (match(0, currentArrow, 'ArrowDown', event)
+        || match(1, currentArrow, 'ArrowLeft', event)
+        || match(2, currentArrow, 'ArrowUp', event)
+        || match(3, currentArrow, 'ArrowRight', event))) {
+        score = score + 10;
     } else {
         console.log("failure");
     }
+    document.getElementById("player-one-score").innerHTML = score;
 });
 
-
-
+function match(direction, currentArrow, keyCode, event) {
+    return currentArrow.direction === direction && event.code === keyCode;
+}
 
